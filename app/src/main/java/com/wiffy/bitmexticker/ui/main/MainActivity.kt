3495efddb77e4658bb01ab227eba95c46ffb22b5
PaleCosmos <tkdgusdlqhek@hanmail.net>
2019-06-20 19:23:09 +0900

    20190620/7:23PM

--- app/src/main/java/com/wiffy/bitmexticker/ui/main/MainActivity.kt
+++ app/src/main/java/com/wiffy/bitmexticker/ui/main/MainActivity.kt
@@ -27,6 +27,7 @@ import kotlinx.android.synthetic.main.app_bar_main.*
 import java.lang.Exception
 import com.google.android.gms.ads.AdRequest
 import com.wiffy.bitmexticker.R
+import java.util.*
 import kotlin.collections.ArrayList
 
 
@@ -42,28 +43,45 @@ class MainActivity : AppCompatActivity(), MainContract.View {
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
 
-        MobileAds.initialize(this,"ca-app-pub-0355430122346055~1344719802")
+        MobileAds.initialize(this, "ca-app-pub-0355430122346055~1344719802")
         val adRequest = AdRequest.Builder().build()
         adView.loadAd(adRequest)
 
         agreement()
         init_loading()
-        mPresenter = MainPresenter(this,applicationContext)
+        mPresenter = MainPresenter(this, applicationContext)
         mPresenter.changeUI()
         mPresenter.getCoin(intent.getStringExtra("symbol"))
+        
         //test
         val strings = ArrayList<String>()
         strings.add("안녕 난 최고")
         strings.add("안녕 너도 최고")
         strings.add("안녕 우리는 최고")
-        viewPager.adapter = ViewPagerAdapter(strings,applicationContext,this)
+        viewPager.adapter = ViewPagerAdapter(strings, applicationContext, this)
         viewPager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+        val newHandler = Handler()
+        var currentPage = 0
+        val update = Runnable {
+            if (currentPage == strings.size) currentPage = 0
+            Handler(Looper.getMainLooper()).post {
+                viewPager.setCurrentItem(currentPage, true)
+                currentPage += 1
+            }
+        }
+
+        val mTimer = Timer()
+        mTimer.schedule(object:TimerTask(){
+            override fun run() {
+                newHandler.post(update)
+            }
+        },550,3300) //3.3초마다 550의 속도로 바뀜
         //test
     }
 
-    private fun agreement(){
-        if (!Util.sharedPreferences.getBoolean("agreement", false)){
-            val builder = AlertDialog.Builder(this,getDialog())
+    private fun agreement() {
+        if (!Util.sharedPreferences.getBoolean("agreement", false)) {
+            val builder = AlertDialog.Builder(this, getDialog())
             builder.setTitle(R.string.Argument)
             builder.setMessage(R.string.argumentContext)
             builder.setPositiveButton("OK") { _, _ ->
@@ -84,7 +102,7 @@ class MainActivity : AppCompatActivity(), MainContract.View {
 
     override fun setRecycler(init_coin: ArrayList<CoinInfo>) {
         Handler(applicationContext.mainLooper).post {
-            myAdapter = MainAdapter(init_coin, this, Util.dark_theme,this)
+            myAdapter = MainAdapter(init_coin, this, Util.dark_theme, this)
             recycler.adapter = myAdapter
             recycler.layoutManager = LinearLayoutManager(this)
         }
@@ -115,7 +133,7 @@ class MainActivity : AppCompatActivity(), MainContract.View {
         trd_info.setTextColor(ContextCompat.getColorStateList(applicationContext, darkAndLight()))
     }
 
-    override fun changeRecent(str:String){
+    override fun changeRecent(str: String) {
         trd_info.text = str
     }
 
@@ -128,7 +146,7 @@ class MainActivity : AppCompatActivity(), MainContract.View {
     }
 
     override fun moveToSetting() {
-        if(setting_on) {
+        if (setting_on) {
             setting_on = false
             startActivity(Intent(this@MainActivity, SettingActivity::class.java))
             overridePendingTransition(R.anim.rightin_activity, R.anim.leftout_activity)
@@ -141,21 +159,25 @@ class MainActivity : AppCompatActivity(), MainContract.View {
         startActivity(intents)
         overridePendingTransition(R.anim.rightin_activity, R.anim.leftout_activity)
     }
+
     override fun setRequestedOrientation(requestedOrientation: Int) {
         if (Build.VERSION.SDK_INT != Build.VERSION_CODES.O) {
             super.setRequestedOrientation(requestedOrientation)
         }
     }
 
-    override fun startLoading(){
-        if(!checkLoading()){
+    override fun startLoading() {
+        if (!checkLoading()) {
             Handler(Looper.getMainLooper()).post {
-                try { builder?.show() }catch (e:Exception){ }
+                try {
+                    builder?.show()
+                } catch (e: Exception) {
+                }
             }
         }
     }
 
-    fun init_loading(){
+    fun init_loading() {
         builder = Dialog(this)
         builder?.setContentView(R.layout.waitting_dialog)
         builder?.setCancelable(false)
@@ -164,15 +186,16 @@ class MainActivity : AppCompatActivity(), MainContract.View {
         startLoading()
     }
 
-    override fun stopLoading(){
-        if (builder?.isShowing!!){
+    override fun stopLoading() {
+        if (builder?.isShowing!!) {
             builder?.dismiss()
         }
     }
 
-    override fun checkLoading():Boolean{
+    override fun checkLoading(): Boolean {
         return builder?.isShowing!!
     }
+
     override fun attachBaseContext(newBase: Context?) {
 
         super.attachBaseContext(
