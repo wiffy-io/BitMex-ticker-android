12a07d4812f328c9c53c12a150bd7fe4635b71fe
PaleCosmos <tkdgusdlqhek@hanmail.net>
2019-07-31 14:25:03 +0900

    20190731/2:24PM

--- app/src/main/java/io/wiffy/bitmexticker/ui/main/VerticalSpaceItemDecoration.kt
+++ app/src/main/java/io/wiffy/bitmexticker/ui/main/VerticalSpaceItemDecoration.kt
@@ -6,7 +6,7 @@ import androidx.recyclerview.widget.RecyclerView
 
 class VerticalSpaceItemDecoration (verticalSpaceHeight : Int): RecyclerView.ItemDecoration() {
 
-    private var verticalSpaceHeight = verticalSpaceHeight;
+    private var verticalSpaceHeight = verticalSpaceHeight
 
     override fun getItemOffsets(outRect : Rect, view : View, parent: RecyclerView, state : RecyclerView.State ) {
         // 마지막 아이템이 아닌 경우, 공백 추가
